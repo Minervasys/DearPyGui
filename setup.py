14738a253dae6aba3a034f1fcaac981c77e30b3a
Fabio Spanò <fabio.spano@minervasys.tech>
2024-04-29 10:01:19 +0200

    core: upgrade project version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import sys
 import shutil
 import subprocess
 
-wip_version = "2.0.0b1"
+wip_version = "2.0.1b1"
 
 def version_number():
     """This function reads the version number which is populated by github actions"""
